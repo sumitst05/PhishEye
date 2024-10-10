508a7a3d785b441b9d6f598a1c247ccbb60a93e4
sumitst05 <sumitst05@gmail.com>
2024-10-11 00:27:33 +0530

    updated cors

--- server/app/main.py
+++ server/app/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import router
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
+
 
 @app.get("/")
 def handle_root():
-    return JSONResponse(
-        status_code=200,
-        content={"message": "Server is running!"}
-    )
+    return JSONResponse(status_code=200, content={"message": "Server is running!"})
